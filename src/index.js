10b8402100d598724ada42c5382d24a39d9bb36b
MazurKateryna <myEmail@example.com>
2020-11-04 15:04:58 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 import './style.scss';
 
 
+import libraryItems from './templates/library.hbs'
+import homePage from './templates/homePage.hbs'
 
-alert("Hello! This is test js");
+const refs = {
+  homePageBtn: document.querySelector('#home'),
+  homePageList: document.querySelector('.main__list'),
+  libraryBtn: document.querySelector('#mylibrary'),
+  libraryList: document.querySelector('.library__list'),
+  mainPage: document.querySelector('.main')
+}
+
+
+function markupLibrary(arr) {
+  refs.homePageList.innerHTML = '';
+  refs.mainPage.classList.add('js-display-none')
+
+  refs.libraryList.insertAdjacentHTML('beforeend', libraryItems(arr));
+}
+
+function markupHomePage(arr) {
+  refs.libraryList.innerHTML = '';
+
+  refs.mainPage.classList.remove('js-display-none')
+  refs.libraryList.classList.add('js-display-none')
+  renderHomePage();
+}
+
+function renderHomePage(arr) {
+  refs.homePageList.insertAdjacentHTML('beforeend', homePage(arr));
+}
+
+refs.homePageBtn.addEventListener('click', markupHomePage);
+refs.libraryBtn.addEventListener('click', markupLibrary);
