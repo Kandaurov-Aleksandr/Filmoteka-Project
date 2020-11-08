c6a20770de0481c35be50b6461c5fcf1261dfa8e
MazurKateryna <myEmail@example.com>
2020-11-08 17:59:00 +0200

    change library.scss

--- src/index.js
+++ src/index.js
@@ -4,32 +4,35 @@ import './style.scss';
 import libraryItems from './templates/library.hbs'
 import homePage from './templates/homePage.hbs'
 
+
 const refs = {
   homePageBtn: document.querySelector('#home'),
   homePageList: document.querySelector('.main__list'),
   libraryBtn: document.querySelector('#mylibrary'),
   libraryList: document.querySelector('.library__list'),
-  mainPage: document.querySelector('.main')
+  mainPage: document.querySelector('.main'),
+  libraryPage: document.querySelector('.library'),
 }
 
+// window.addEventListener("load", ()=> {
+//   return renderHomePage();
+// }
 
-function markupLibrary(arr) {
+function markupLibrary() {
   refs.homePageList.innerHTML = '';
-  refs.mainPage.classList.add('js-display-none')
+  refs.mainPage.classList.add('js-display-none');
+  refs.libraryPage.classList.remove('js-display-none')
 
-  refs.libraryList.insertAdjacentHTML('beforeend', libraryItems(arr));
+
+  return refs.libraryList.insertAdjacentHTML('beforeend', libraryItems());
 }
 
-function markupHomePage(arr) {
+function markupHomePage() {
   refs.libraryList.innerHTML = '';
 
   refs.mainPage.classList.remove('js-display-none')
-  refs.libraryList.classList.add('js-display-none')
-  renderHomePage();
-}
-
-function renderHomePage(arr) {
-  refs.homePageList.insertAdjacentHTML('beforeend', homePage(arr));
+  refs.libraryPage.classList.add('js-display-none')
+  return refs.homePageList.insertAdjacentHTML('beforeend', homePage());
 }
 
 refs.homePageBtn.addEventListener('click', markupHomePage);
