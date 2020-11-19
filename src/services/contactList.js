b03cb5d2871ec5b105d7ca04d81d80be1e2b55a2
Aleksandr <kandayrov1990@gmail.com>
2020-11-20 00:54:35 +0200

    add team-card

--- src/services/contactList.js
+++ src/services/contactList.js
@@ -1,13 +1,18 @@
 import contactsTemplate from "../templates/contactsTeam.hbs";
 import contacts from "../components/contacts";
 import refs from "../services/refs";
+import Pagination from "../components/Pagination";
+
+
 
 
 const markup = contactsTemplate(contacts);
 
 function contactsMarkup() {
   refs.main.innerHTML = "";
-  refs.main.insertAdjacentHTML('beforeend', markup)
+  refs.main.insertAdjacentHTML('beforeend', markup);
+  Pagination.clear();
 }
 
 refs.footerBtn.addEventListener('click', contactsMarkup);
+
